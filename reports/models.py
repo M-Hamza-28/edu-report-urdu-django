ebab51aedd4f2b4730ccca0e74ab5375bee531e4
M-Hamza-28 <engr.mohammadhamzanadeem@gmail.com>
2025-08-19 00:22:13 +0500

    mig fix

--- reports/models.py
+++ reports/models.py
@@ -68,8 +68,10 @@ class StudentSession(models.Model):
 class Exam(models.Model):
     name = models.CharField(max_length=100)
     exam_type = models.CharField(max_length=50)
-    session = models.ForeignKey('ExamSession', on_delete=models.CASCADE, related_name='exams')
+    # session = models.ForeignKey('ExamSession', on_delete=models.CASCADE, related_name='exams')
+    session = models.ForeignKey('AcademicSession', null=True, blank=True, on_delete=models.PROTECT)
     date = models.DateField()
+
 
     def __str__(self):
         return f"{self.name} ({self.exam_type})"
